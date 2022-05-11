7a779f8995df6df4b8bc4412ca6eaa3d51f962ce
Gabriel Dimitrievski <62767938+gabrieldim@users.noreply.github.com>
2022-05-11 22:47:34 +0200

    Add files via upload

---
+++ contracts/Greeter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+//lab05
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FinkiCoin is ERC20 {
+
+    constructor() ERC20("Finki Coin", "FINKI") {
+        _mint(msg.sender, 1000000e18);
+    }
+
+}
